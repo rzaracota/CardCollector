4ca83cdfe6bc2177bdccdd1088693ecdcd653685
Grey Gilroy <grey.gilroy@gmail.com>
2017-07-02 10:39:15 -0700

    Create an element function for individual navigation elements.
    In this process we learned that all lower case tags are treated
    as pure xml/html.

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
+function NElement(props) {
+    return (<li><a href={props.target} className="text-capitalize">{props.target}</a></li>);
+}
+
 class Navigator extends Component {
     render() {
+	var navItems = [];
+
+	for (var i = 0; i < this.props.navItems.length; i++) {
+	    navItems.push(<NElement target={this.props.navItems[i]} />);
+	}
+	
 	return ( <div className="navbar navbar-inverse">
   <div className="container-fluid">
   <div className="navbar-header">
     <a className="navbar-brand">Card Collector</a>
   </div>
 		 <ul className="nav navbar-nav navbar-right">
-		 {this.props.navItems}
+		 {navItems}
+		 <li>Items: {navItems.length}</li>
 		 <li><a href="#">Home</a></li>
 		 <li><a href="#">Sign up now!</a></li>
 		 <li><a href="#">Card Collections</a></li>
