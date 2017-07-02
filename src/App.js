a90d61c243c92f33ecf90cdac6bbbb55ef524557
Grey Gilroy <grey.gilroy@gmail.com>
2017-07-01 19:12:15 -0700

    Implement navi with bootstrap

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigator from './components/navigator';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h2>Welcome to Card Collector</h2>
+            </div>
+	    <Navigator />
+	    
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/navigator.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Navigator extends Component {
+    render() {
+	return ( <div className="navbar navbar-inverse">
+  <div className="container-fluid">
+  <div className="navbar-header">
+    <a className="navbar-brand">Card Collector</a>
+  </div>
+  <ul className="nav navbar-nav navbar-right">    
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Sign up now!</a></li>
+		 <li><a href="#">Card Collections</a></li>
+		 <li><a href="#">Browse Cards</a></li>
+		 		   <li><a href="#">Browse People</a></li>
+  <li><a href="#">User Index</a></li>
+  <li><a href="#">Log in</a></li>
+  <li><a href="#">Log out</a></li>
+  </ul>
+  </div>
+</div>	);
+    }
+}
+
+export default Navigator;
