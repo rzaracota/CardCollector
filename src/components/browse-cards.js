bd55ecc21fe197f9533194e73a3dcf435a76205a
Grey Gilroy <grey.gilroy@gmail.com>
2017-07-03 23:09:42 -0700

    Create a basic cardbrief element

--- src/components/browse-cards.js
+++ src/components/browse-cards.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 
 import CardDB from '../lib/card-database';
 
+class CardBrief extends Component {
+    render() {
+	return (<p>{this.props.cards}</p>);
+    }
+}
+
 class BrowseCards extends Component {
     render() {
-	return (<h3>Browse cards</h3>);
+	var db = new CardDB();
+	var cards = db.getCards();
+
+	if (cards) {
+	    var cardBriefs = [];
+
+	    for (var i = 0; i < cards.length; i++) {
+		cardBriefs.push(<CardBrief card={cards[i]} />);
+	    }
+	    
+	    return (<li>
+		    {cardBriefs}
+		    </li>);
+	} else {
+	    return (<h3>No cards in db</h3>);
+	}
     }
 }
 
--- src/lib/card-database.js
+++ src/lib/card-database.js
@@ -1,4 +1,22 @@
+class Card {
+    constructor() {
+	this.name = "Kittenator";
+	this.type = "Sandfire";
+	this.hp = "135";
+	this.descriptions = "Though cute as a button and equally loving, \
+the kittenator would just as easily melt your face off when it is tired of \
+your attention";
+    }
+}
+
 class CardDB {
+    constructor() {
+	var cards = [ new Card(), new Card() ];
+    }
+
+    getCards() {
+	return this.cards;
+    }
 }
 
 export default CardDB;
