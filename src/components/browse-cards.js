56afce1cc572aa2d8d295e0ee163e8a81f44acd6
Grey Gilroy <grey.gilroy@gmail.com>
2017-07-04 15:22:25 -0700

    Github seems to require a relative path for the image

--- src/components/browse-cards.js
+++ src/components/browse-cards.js
@@ -5,7 +5,7 @@ import './card.css';
 
 class CardBrief extends Component {
     render() {
-	return (<div className="col-md-8 col-md-offset-2"><div className="row card-brief"><div className="col-md-1"><img className="image-responsive browse-image" src="/images/kittenator.png" alt="alt text" /></div><div className="col-md-11"><p className="text-left">{this.props.itemNum}: {this.props.card.name}: Lvl: {this.props.card.lvl}</p></div></div></div>);
+	return (<div className="col-md-8 col-md-offset-2"><div className="row card-brief"><div className="col-md-1"><img className="image-responsive browse-image" src="images/kittenator.png" alt="alt text" /></div><div className="col-md-11"><p className="text-left">{this.props.itemNum}: {this.props.card.name}: Lvl: {this.props.card.lvl}</p></div></div></div>);
     }
 }
 
